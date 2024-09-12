7849a2164955ec3def399baa9a78d7c59d5cad1a
CloudreiL <annaf5664@gmail.com>
2024-09-12 15:42:38 +0300

    Edited Notification Page

--- lib/classes/style.dart
+++ lib/classes/style.dart
@@ -1,47 +1,53 @@
 import 'package:flutter/material.dart';
 
-
-class TextStyles{
-    static const TextStyle StyleText = TextStyle(
-      fontSize: 20,
-      fontFamily: 'FiraSans_Regular',
-      color: Colors.white
-  );
+class TextStyles {
+  static const TextStyle StyleText = TextStyle(
+      fontSize: 20, fontFamily: 'FiraSans_Regular', color: Colors.white);
 }
 
-class TextFields{
+class TextFields {
   static InputDecoration FieldDec = InputDecoration(
-      enabledBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.white),
-          borderRadius: BorderRadius.circular(20)
-      ),
-      focusedBorder: OutlineInputBorder(
-          borderSide: BorderSide(color: Colors.white),
-          borderRadius: BorderRadius.circular(20)
-      ),
-      labelStyle: TextStyles.StyleText,
-      filled: true,
-      fillColor: Color.fromRGBO(255, 255, 255, 200),
-
+    enabledBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.white),
+        borderRadius: BorderRadius.circular(20)),
+    focusedBorder: OutlineInputBorder(
+        borderSide: BorderSide(color: Colors.white),
+        borderRadius: BorderRadius.circular(20)),
+    labelStyle: TextStyles.StyleText,
+    filled: true,
+    fillColor: Color.fromRGBO(255, 255, 255, 200),
   );
 }
 
-class ContainerDecor{
+class ContainerDecor {
   static BoxDecoration ContainerDec = BoxDecoration(
       gradient: LinearGradient(
         begin: Alignment.centerLeft,
         end: Alignment.centerRight,
-        colors: [Color.fromRGBO(103, 58, 183, 1), Color.fromRGBO(195, 66, 218, 1)],
+        colors: [
+          Color.fromRGBO(103, 58, 183, 1),
+          Color.fromRGBO(195, 66, 218, 1)
+        ],
       ),
-      boxShadow: [BoxShadow(
-          color: Colors.black.withOpacity(0.5),
-          offset: Offset(0,3),
-          spreadRadius: 1,
-          blurRadius: 10
-        )
+      boxShadow: [
+        BoxShadow(
+            color: Colors.black.withOpacity(0.5),
+            offset: Offset(0, 3),
+            spreadRadius: 1,
+            blurRadius: 10)
       ],
-      borderRadius: BorderRadius.circular(15)
-  );
+      borderRadius: BorderRadius.circular(15));
+}
+
+class DropDownMenuDec {
+  static InputDecorationTheme MenuDec = InputDecorationTheme(
+      isDense: true,
+      border: OutlineInputBorder(borderRadius: BorderRadius.circular(40)),
+      labelStyle: TextStyles.StyleText.copyWith(color: Colors.black),
+      fillColor: Colors.white,
+      filled: true,
+      counterStyle: TextStyles.StyleText,
+      contentPadding: EdgeInsets.only(left: 20));
 }
 
 class PieData {
@@ -50,13 +56,52 @@ class PieData {
   final Icon icon;
   final String title;
 
-  PieData({required this.value, required this.color, required this.icon, required this.title});
+  PieData(
+      {required this.value,
+      required this.color,
+      required this.icon,
+      required this.title});
 }
 
 final dataChart = [
-  PieData(value: 10, color: Color.fromRGBO(134, 216, 120, 1), icon: Icon(Icons.sentiment_satisfied_alt, color: Colors.black,), title: ''),
-  PieData(value: 20, color: Color.fromRGBO(140, 248, 255, 1), icon: Icon(Icons.sentiment_dissatisfied_outlined, color: Colors.black,), title: ''),
-  PieData(value: 30, color: Color.fromRGBO(255, 178, 178, 1), icon: Icon(Icons.sentiment_very_dissatisfied_outlined, color: Colors.black,), title: ''),
-  PieData(value: 40, color: Color.fromRGBO(255, 244, 142, 1), icon: Icon(Icons.sentiment_very_satisfied_rounded, color: Colors.black,), title: ''),
-  PieData(value: 50, color: Colors.white, icon: Icon(Icons.sentiment_neutral_outlined, color: Colors.black,), title: '')
+  PieData(
+      value: 10,
+      color: Color.fromRGBO(134, 216, 120, 1),
+      icon: Icon(
+        Icons.sentiment_satisfied_alt,
+        color: Colors.black,
+      ),
+      title: ''),
+  PieData(
+      value: 20,
+      color: Color.fromRGBO(140, 248, 255, 1),
+      icon: Icon(
+        Icons.sentiment_dissatisfied_outlined,
+        color: Colors.black,
+      ),
+      title: ''),
+  PieData(
+      value: 30,
+      color: Color.fromRGBO(255, 178, 178, 1),
+      icon: Icon(
+        Icons.sentiment_very_dissatisfied_outlined,
+        color: Colors.black,
+      ),
+      title: ''),
+  PieData(
+      value: 40,
+      color: Color.fromRGBO(255, 244, 142, 1),
+      icon: Icon(
+        Icons.sentiment_very_satisfied_rounded,
+        color: Colors.black,
+      ),
+      title: ''),
+  PieData(
+      value: 50,
+      color: Colors.white,
+      icon: Icon(
+        Icons.sentiment_neutral_outlined,
+        color: Colors.black,
+      ),
+      title: '')
 ];
--- lib/pages/notification_page.dart
+++ lib/pages/notification_page.dart
@@ -1,36 +1,213 @@
-import 'package:flutter/cupertino.dart';
+import 'package:dreamy_project/classes/style.dart';
 import 'package:flutter/material.dart';
 
-import 'package:dreamy_project/classes/style.dart';
-
-class NotificationPage extends StatefulWidget{
+class NotificationPage extends StatefulWidget {
   const NotificationPage({super.key});
 
   @override
   _NotificationPageState createState() => _NotificationPageState();
-
 }
 
-class _NotificationPageState extends State<NotificationPage>{
-
+class _NotificationPageState extends State<NotificationPage> {
   @override
-  Widget build(BuildContext context){
+  bool light0 = true;
+  bool light1 = true;
+  bool light2 = true;
+
+  List<String> time0 = ["9PM", "10PM", "11PM", "12PM"];
+  List<String> time1 = ["6AM", "7AM", "8AM", "9AM"];
+  String? dropdownValue0;
+  String? dropdownValue1;
+  bool dropDownMenu0 = false;
+  bool dropDownMenu1 = false;
+
+  Widget build(BuildContext context) {
     return Container(
       decoration: BoxDecoration(
           gradient: LinearGradient(
-            begin: Alignment.topCenter,
-            end: Alignment.bottomCenter,
-            colors: [Colors.black, Color.fromRGBO(57, 34, 99, 1)],
-          )
-      ),
-
+        begin: Alignment.topCenter,
+        end: Alignment.bottomCenter,
+        colors: [Colors.black, Color.fromRGBO(57, 34, 99, 1)],
+      )),
       child: Scaffold(
         backgroundColor: Colors.transparent,
         appBar: AppBar(
-          title: Text('Notifications', style: TextStyles.StyleText.copyWith(
-            fontSize: 30
-          )),
-            backgroundColor: Colors.transparent
+            title: Text('Notifications',
+                style: TextStyles.StyleText.copyWith(fontSize: 30)),
+            backgroundColor: Colors.transparent),
+        body: Center(
+          child: Column(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width * 1,
+                margin: EdgeInsets.all(20),
+                height: 50,
+                decoration: ContainerDecor.ContainerDec.copyWith(
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(
+                        'Bedtime notifications',
+                        style: TextStyles.StyleText,
+                      ),
+                    ),
+                    Switch(
+                      activeColor: Colors.white,
+                      value: light0,
+                      onChanged: (bool value) {
+                        setState(() {
+                          light0 = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 1,
+                margin: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                height: 50,
+                decoration: ContainerDecor.ContainerDec.copyWith(
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(
+                        'Morning notifications',
+                        style: TextStyles.StyleText,
+                      ),
+                    ),
+                    Switch(
+                      activeColor: Colors.white,
+                      value: light1,
+                      onChanged: (bool value) {
+                        setState(() {
+                          light1 = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 1,
+                margin: EdgeInsets.only(left: 20, right: 20, bottom: 20),
+                height: 50,
+                decoration: ContainerDecor.ContainerDec.copyWith(
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: EdgeInsets.only(left: 10),
+                      child: Text(
+                        'Sound on',
+                        style: TextStyles.StyleText,
+                      ),
+                    ),
+                    Switch(
+                      activeColor: Colors.white,
+                      value: light2,
+                      onChanged: (bool value) {
+                        setState(() {
+                          light2 = value;
+                        });
+                      },
+                    ),
+                  ],
+                ),
+              ),
+              Text(
+                'Notification time',
+                style: TextStyles.StyleText.copyWith(fontSize: 30),
+              ),
+              Text(
+                'Send me notifications',
+                style: TextStyles.StyleText,
+              ),
+              Container(
+                width: MediaQuery.of(context).size.width * 1,
+                margin: EdgeInsets.only(left: 20, right: 20, top: 15),
+                height: 70,
+                decoration: ContainerDecor.ContainerDec.copyWith(
+                    borderRadius: BorderRadius.circular(20)),
+                child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.only(left: 8.0),
+                        child: Text('from', style: TextStyles.StyleText),
+                      ),
+                      Container(
+                        width: 115,
+                        decoration: BoxDecoration(boxShadow: [
+                          BoxShadow(
+                              color: Colors.black.withOpacity(0.5),
+                              offset: Offset(0, 3),
+                              spreadRadius: 1,
+                              blurRadius: 10)
+                        ], borderRadius: BorderRadius.circular(40)),
+                        child: DropdownMenu(
+                          width: 115,
+                          dropdownMenuEntries: time0
+                              .map<DropdownMenuEntry<String>>((String value) {
+                            return DropdownMenuEntry<String>(
+                                value: value, label: value);
+                          }).toList(),
+                          textStyle: TextStyles.StyleText.copyWith(
+                              color: Colors.black),
+                          inputDecorationTheme: DropDownMenuDec.MenuDec,
+                          initialSelection: dropdownValue0,
+                          onSelected: (String? newValue0) {
+                            setState(() {
+                              dropdownValue0 = newValue0;
+                            });
+                          },
+                        ),
+                      ),
+                      Text('to', style: TextStyles.StyleText),
+                      Padding(
+                        padding: const EdgeInsets.only(right: 8.0),
+                        child: Container(
+                          width: 115,
+                          decoration: BoxDecoration(boxShadow: [
+                            BoxShadow(
+                                color: Colors.black.withOpacity(0.5),
+                                offset: Offset(0, 3),
+                                spreadRadius: 1,
+                                blurRadius: 10)
+                          ], borderRadius: BorderRadius.circular(40)),
+                          child: DropdownMenu(
+                            width: 115,
+                            dropdownMenuEntries: time1
+                                .map<DropdownMenuEntry<String>>((String value) {
+                              return DropdownMenuEntry<String>(
+                                  value: value, label: value);
+                            }).toList(),
+                            textStyle: TextStyles.StyleText.copyWith(
+                                color: Colors.black),
+                            inputDecorationTheme: DropDownMenuDec.MenuDec,
+                            initialSelection: dropdownValue1,
+                            onSelected: (String? newValue1) {
+                              setState(() {
+                                dropdownValue1 = newValue1;
+                              });
+                            },
+                          ),
+                        ),
+                      ),
+                    ]),
+              )
+            ],
+          ),
         ),
       ),
     );
